6fb0bfbe5cee613c27098be32e270ac93bb994f3
theaao <ao_xinyan@163.com>
2025-09-18 10:15:12 +0200

    add click manager

--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ClickManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("ClickManager started.");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Debug.Log("Mouse Left Button was pressed this frame.");
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                IClickable clickable = hit.collider.GetComponent<IClickable>();
+                if (clickable != null)
+                {
+                    clickable.OnClick();
+                }
+            }
+        }
     }
+}
+
+public interface IClickable
+{
+    void OnClick();
 }
